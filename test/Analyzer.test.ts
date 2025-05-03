a9e419254806af1269ee634af9f3b9bf7be7f829
Yuqi Liu <yuqiliu617@outlook.com>
2025-05-02 20:26:13 -0700

    refactor: test case skip

--- test/Analyzer.test.ts
+++ test/Analyzer.test.ts
@@ -26,28 +26,31 @@ const cases: TestCase[] = [];
 describe("Reentrancy Analyzer", () => {
 	const debugProviders = new Map<ChainName, DebugTraceProvider>();
 
-	test.each(cases.filter(t => t.skip !== true))("$name on $chain", async testCase => {
-		const { chain, txHash } = testCase;
-		let provider = debugProviders.get(chain);
-		if (!provider) {
-			if (!(chain in tenderlyNodeAccessKeys))
-				throw new Error(`No Tenderly access key for ${chain}`);
-			// @ts-ignore
-			provider = new Tenderly(chain, tenderlyNodeAccessKeys[chain]);
-			debugProviders.set(chain, provider);
-		}
-		const analyzer = new Reentrancy.Analyzer(chain, etherscanApiKey, provider);
-		let detected = false;
-		for await (const result of analyzer.analyze(txHash)) {
-			detected = true;
-			if (!testCase.isReentrancy)
-				fail(`Expected no reentrancy, but got ${result.stack}`);
-			else {
-				if (testCase.scope)
-					expect(result.scope).toBe(testCase.scope);
-				console.log(Reentrancy.Analyzer.toString(result));
+	for (const testCase of cases) {
+		const it = testCase.skip ? test.skip : test;
+		it(`${testCase.name} on ${testCase.chain}`, async () => {
+			const { chain, txHash } = testCase;
+			let provider = debugProviders.get(chain);
+			if (!provider) {
+				if (!(chain in tenderlyNodeAccessKeys))
+					throw new Error(`No Tenderly access key for ${chain}`);
+				// @ts-ignore
+				provider = new Tenderly(chain, tenderlyNodeAccessKeys[chain]);
+				debugProviders.set(chain, provider);
 			}
-		}
-		expect(detected).toBe(testCase.isReentrancy);
-	}, 30_000);
+			const analyzer = new Reentrancy.Analyzer(chain, etherscanApiKey, provider);
+			let detected = false;
+			for await (const result of analyzer.analyze(txHash)) {
+				detected = true;
+				if (!testCase.isReentrancy)
+					fail(`Expected no reentrancy, but got ${result.stack}`);
+				else {
+					if (testCase.scope)
+						expect(result.scope).toBe(testCase.scope);
+					console.log(Reentrancy.Analyzer.toString(result));
+				}
+			}
+			expect(detected).toBe(testCase.isReentrancy);
+		}, 30_000);
+	}
 });
