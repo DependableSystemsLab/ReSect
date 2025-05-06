b55504cc75e9d590405d94543d722342f962356c
Yuqi Liu <yuqiliu617@outlook.com>
2025-05-06 05:10:19 -0700

    test: add test case option `useDatabase`

--- test/Analyzer.test.ts
+++ test/Analyzer.test.ts
@@ -1,15 +1,16 @@
 import inspector from "node:inspector";
 import { Chain, type ChainName } from "../src/config/Chain";
 import { etherscanApiKey, quickNodeApiKey, tenderlyNodeAccessKeys } from "../src/config/credentials";
-import { Etherscan, QuickNodeWithDb, TenderlyWithDb, type DebugTraceProvider } from "../src/providers";
+import { Database } from "../src/database";
+import { Etherscan, QuickNode, QuickNodeWithDb, Tenderly, TenderlyWithDb, type DebugTraceProvider } from "../src/providers";
 import { Reentrancy } from "../src/Reentrancy";
 import type { Hex } from "../src/utils";
-import { Database } from "../src/database";
 
 interface TestCaseBase {
 	name: string;
 	chain: ChainName;
 	txHash: Hex.String;
+	useDatabase?: boolean;
 	skip?: boolean;
 }
 
@@ -30,17 +31,24 @@ describe("Reentrancy Analyzer", () => {
 	const debug = inspector.url() !== undefined;
 	const timeout = debug ? 24 * 60 * 60_000 : 30_000;
 
-	const etherscan = new Etherscan(etherscanApiKey, Chain.Ethereum, Database.default);
+	const etherscan = new Etherscan(etherscanApiKey, Chain.Ethereum);
 	const debugProvider: DebugTraceProvider = quickNodeApiKey
+		? new QuickNode(quickNodeApiKey, "Ethereum")
+		: new Tenderly(tenderlyNodeAccessKeys, "Ethereum");
+
+	const etherscanWithDb = new Etherscan(etherscanApiKey, Chain.Ethereum, Database.default);
+	const debugProviderWithDb: DebugTraceProvider = quickNodeApiKey
 		? new QuickNodeWithDb(quickNodeApiKey, "Ethereum")
-		: new TenderlyWithDb(tenderlyNodeAccessKeys, "Ethereum", etherscan.geth);
+		: new TenderlyWithDb(tenderlyNodeAccessKeys, "Ethereum", etherscanWithDb.geth);
 
 	for (const testCase of cases) {
 		const it = testCase.skip ? test.skip : test;
 		it(`${testCase.name} on ${testCase.chain}`, async () => {
 			const { chain, txHash } = testCase;
 			const chainId = Chain[chain];
-			const analyzer = new Reentrancy.Analyzer(etherscan, debugProvider);
+			const analyzer = testCase.useDatabase === false
+				? new Reentrancy.Analyzer(etherscan, debugProvider)
+				: new Reentrancy.Analyzer(etherscanWithDb, debugProviderWithDb);
 			let detected = false;
 			for await (const result of analyzer.analyze(txHash, chainId)) {
 				detected = true;
